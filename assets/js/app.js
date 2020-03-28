930aea7c6e21ea26b546705080ea0e9a8d2c6437
darchoco <56795928+darchoco@users.noreply.github.com>
2020-03-28 13:42:48 -0700

    pushing up comments

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,10 @@
 // @TODO: YOUR CODE HERE!
+
+//declare the svg width
 var svgWidth = 960;
 var svgHeight = 500;
 
+//declare margins to be adjusted
 var margin = {
   top: 20,
   right: 40,
@@ -9,30 +12,25 @@ var margin = {
   left: 100
 };
 
-
+//set actual height and width using declared above
 var height = svgHeight - margin.top - margin.bottom;
 var width = svgWidth - margin.left - margin.right;
 
+//selecting the location for the graph, applying the attributes
 var scatter = d3.select("#scatter")
 .append("svg")
 .attr("height", svgHeight)
 .attr("width", svgWidth);
 
+//transforming the declared graph to match with margins created
 var chartGroup = scatter.append("g")
 .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
+
+//calling csv data to pull
 d3.csv("assets/data/data.csv").then(function(Data) {
-    // var id = Data.map(data => data.id);
-    // console.log("id", id);
-    // var state = Data.map(data => data.state);
-    // console.log("state", state);
-     var abbr = Data.map(data => data.abbr);
-    // console.log("abbr", abbr);
-    // var age = Data.map(data => data.age);
-    // console.log("age", age);
-    // var obesity = Data.map(data => data.obesity);
-    // console.log("obesity", obesity);
-  
+
+//converting needed data into numbers versus strings  
     console.log(Data);
     Data.forEach(function(data) {
         data.age = +data.age;
@@ -40,7 +38,7 @@ d3.csv("assets/data/data.csv").then(function(Data) {
         
       });
 
-  // scales
+  // setting scales, adjusted for actual values
 var xScale = d3.scaleLinear()
 .domain([28, d3.max(Data, d=>d.age)])
 .range([28, width]);
@@ -49,20 +47,33 @@ var yScale = d3.scaleLinear()
 .domain([20, d3.max(Data, d=>d.obesity)])
 .range([height, 20]);
 
-    // Step 3: Create axis functions
-    // ==============================
+//create axis function to starting appending using scales
 var bottomAxis = d3.axisBottom(xScale);
 var leftAxis = d3.axisLeft(yScale);
 
+//transforming with axis
 chartGroup.append("g")
 .attr("transform", `translate(0, ${height})`)
 .call(bottomAxis);
-
+//transforming with axis
 chartGroup.append("g")
 .call(leftAxis);
 
 
 
+
+//set text labels first to be able to click on circles
+//use xScale and Yscale to graph appropriately on svg
+var labels = chartGroup.selectAll("labels")
+.data(Data)
+.enter()
+.append("text")
+.attr("x",d => xScale(d.age)-10)
+.attr("y",d => yScale(d.obesity)+5)
+.text(d=>d.abbr)
+
+//apply the data, age on x, obesity on y
+//opacity set to allow for text labels to be seen
 var circlesGroup = chartGroup.selectAll("circle")
 .data(Data)
 .enter()
@@ -73,15 +84,25 @@ var circlesGroup = chartGroup.selectAll("circle")
 .attr("fill", "blue")
 .attr("opacity", ".5")
 
-var labels = chartGroup.selectAll("labels")
-.data(Data)
-.enter()
-.append("text")
-.attr("x",d => xScale(d.age)-10)
-.attr("y",d => yScale(d.obesity)+5)
-.text(d=>d.abbr)
+//set up tooltip to allow for circles to be clicked on and show exact data
+    var toolTip = d3.tip()
+      .attr("class", "tooltip")
+      .offset([80, -60])
+      .html(function(d) {
+        return (`${d.state}<br>average age: ${d.age}<br>obesity %: ${d.obesity}`);
+      });
 
+//create toolTip in chart
+    chartGroup.call(toolTip);
 
+//create listener to call on function to pull data
+    circlesGroup.on("click", function(data) {
+      toolTip.show(data, this);
+    })
+      // onmouseout event
+      .on("mouseout", function(data, index) {
+        toolTip.hide(data);
+      });
 
     // Create axes labels
     chartGroup.append("text")
@@ -98,7 +119,7 @@ var labels = chartGroup.selectAll("labels")
       .text("obesity %");
 
 
-
+//in case of error
 }).catch(function(error) {
     console.log(error);
   });
